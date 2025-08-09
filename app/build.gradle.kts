f2b86eb439acf9a6064b2f5e79f10ebf198fe65c
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-08-09 23:39:41 +0530

    Refactor dependencies to use version catalog.
    - Updated `app/build.gradle.kts` to use library aliases from the version catalog.
    - Added versions and library definitions for Coil, Retrofit, Gson converter, and Kotlinx Coroutines Android to `gradle/libs.versions.toml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation("io.coil-kt:coil-compose:2.7.0")                  // Image loading
-    implementation("com.squareup.retrofit2:retrofit:3.0.0")           // Retrofit networking
-    implementation("com.squareup.retrofit2:converter-gson:3.0.0")     // JSON converter for Retrofit
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2") // Coroutines support
-//    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
+    implementation(libs.coil.compose)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.kotlinx.coroutines.android)
 }
