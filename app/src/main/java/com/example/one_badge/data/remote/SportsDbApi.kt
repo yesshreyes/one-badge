3f44bd15497f834f49a168448da4af9d681d5056
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2026-01-26 13:15:55 +0530

    Refactor: Abstract API key from SportsDbApi interface

--- app/src/main/java/com/example/one_badge/data/remote/SportsDbApi.kt
+++ app/src/main/java/com/example/one_badge/data/remote/SportsDbApi.kt
@@ -1,12 +1,15 @@
 package com.example.one_badge.data.remote
 
+import com.example.one_badge.BuildConfig
 import com.example.one_badge.data.models.TeamResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SportsDbApi {
-    @GET("api/v1/json/123/searchteams.php")
+    @GET("api/v1/json/{apiKey}/searchteams.php")
     suspend fun searchTeams(
+        @Path("apiKey") apiKey: String = BuildConfig.SPORTS_DB_API_KEY,
         @Query("t") teamName: String,
     ): TeamResponse
 }
